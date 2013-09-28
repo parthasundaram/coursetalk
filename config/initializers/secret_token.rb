9009783be4a4c58f5d5033e85875ccdce8a8fa77
Partha Sundaram <parthasundaram@gmail.com>
2013-09-27 21:39:18 -0700

    Starting CourseRank

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coursetalk::Application.config.secret_token = '1d9a8c8802f9c7b528a91582e75c1dc76605fa9e2bafe9ccc53eb6b9495334c200cf83f0c20cf44f97c809197bcdf39ce5987b7201533f66143b781788b98c84'
